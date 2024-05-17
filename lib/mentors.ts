de31efb50cf23c9408ad71cdbfe281f57d5c80e7
Ethan Franks <j.ethan.franks@gmail.com>
2024-05-17 09:27:50 -0500

    feat(mentorship): mentor schema

--- lib/mentors.ts
+++ lib/mentors.ts
@@ -1,3 +1,31 @@
-// put code for interacting with mentor database
-// define mentor type - zod schema
-// zod for parsing and validation
+import { z } from "zod";
+
+const MentorSchema = z.object({
+  id: z.string().uuid(),
+  name: z.string({
+    required_error: "Name is required.",
+  }).trim()
+    .min(1, "First Name cannot be empty.")
+    .max(256, "Name cannot contain more than 256 characters.")
+    .regex(
+      /^[A-Za-z\x{00C0}-\x{00FF}][A-Za-z\x{00C0}-\x{00FF}\'\-]+([\ A-Za-z\x{00C0}-\x{00FF}][A-Za-z\x{00C0}-\x{00FF}\'\-]+)*/u,
+      "Name cannot contain numbers or special characters other than hyphens.",
+    ),
+  slackUsername: z.string(),
+  slackId: z.string().optional(),
+  title: z.string(),
+  description: z.string().max(400),
+  tags: z.string({
+    required_error: "Tags cannot be empty.",
+  })
+    .trim()
+    .min(1, "Tags cannot be empty.")
+    .max(400, "Tags cannot contain more than 400 characters.")
+    .regex(/\w+(,\w+)*/g, "Tags must be comma-delimited."),
+  guidelinesAccepted: z.boolean({
+    required_error:
+      "User must accept mentorship guidelines to submit a request to become a mentor.",
+  }),
+  isApproved: z.boolean(),
+});
+export type Mentor = z.infer<typeof MentorSchema>;
